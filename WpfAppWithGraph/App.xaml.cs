ae5f8fdc5b69bf32507cbc2b292b294cc3088a15
Ashish Trivedi <ashuetawah@hotmail.com>
2018-06-14 23:14:25 +1000

    Initial commit

---
+++ WpfAppWithGraph/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+using Microsoft.Identity.Client;
+
+namespace WpfAppWithGraph
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private static string clientid = "Your App Client ID";
+
+        public static PublicClientApplication PublicClientApp = new PublicClientApplication(clientid);
+    }
+}
